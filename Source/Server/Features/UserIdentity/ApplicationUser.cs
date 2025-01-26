2e73dd024bfb34862a2ff59be3cf52d96a11f1a6
David Eggenberger <David.Eggenberger@hotmail.com>
2025-01-26 20:12:10 +0100

    server infrastructure init

---
+++ Source/Server/Features/UserIdentity/ApplicationUser.cs
@@ -0,0 +1,6 @@
+﻿namespace Server.Features.UserIdentity
+{
+    public class ApplicationUser
+    {
+    }
+}
--- Source/Server/Startup.cs
+++ Source/Server/Startup.cs
@@ -1,15 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Server.Features.UserIdentity;
+using Server.Infrastructure.EFCore;
 
 namespace Server
 {
@@ -25,8 +31,59 @@ namespace Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddSignalR();
+            services.AddRazorPages();
             services.AddControllers();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["SQLConnection"]);
+            });
+
+            AuthenticationBuilder authenticationBuilder = services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+                options.RequireAuthenticatedSignIn = false;
+            })
+                .AddMicrosoftAccount(options =>
+                {
+                    options.ClientId = Configuration["LinkedInClientId"];
+                    options.ClientSecret = Configuration["LinkedInClientSecret"];
+                    options.Events.OnTicketReceived = async context =>
+                    {
+                        var kvp = context.Properties.Items[".AuthScheme"];
+                        context.Properties.Items.Add("LoginProvider", kvp);
+                    };
+                })
+                .AddGoogle(options =>
+                {
+                    options.ClientId = Configuration["GoogleClientId"];
+                    options.ClientSecret = Configuration["GoogleClientSecret"];
+                    options.Events.OnCreatingTicket = (context) =>
+                    {
+                        var picture = context.User.GetProperty("picture").GetString();
+                        context.Identity.AddClaim(new Claim("picture", picture));
+                        return Task.CompletedTask;
+                    };
+                    options.Events.OnTicketReceived = async context =>
+                    {
+                        var kvp = context.Properties.Items[".AuthScheme"];
+                        context.Properties.Items.Add("LoginProvider", kvp);
+                    };
+                });
+
+            authenticationBuilder.AddIdentityCookies();
+
+            var identityService = services.AddIdentityCore<ApplicationUser>(options =>
+            {
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+/ ";
+                options.User.RequireUniqueEmail = true;
+                options.Stores.MaxLengthForKeys = 128;
+            })
+                .AddDefaultTokenProviders()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddSignInManager();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,14 +95,18 @@ namespace Server
             }
 
             app.UseHttpsRedirection();
-
+            app.UseStaticFiles();
+            app.UseBlazorFrameworkFiles();
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapRazorPages();
+                endpoints.MapFallbackToPage("/_Host");
             });
         }
     }
