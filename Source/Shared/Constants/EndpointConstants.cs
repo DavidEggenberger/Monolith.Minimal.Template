35446945598242c31901b0a7c57de61038e3b35d
David Eggenberger <David.Eggenberger@hotmail.com>
2025-01-28 19:31:58 +0100

    client BuildingBlocks Auth

---
+++ Source/Shared/Constants/EndpointConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Constants
+{
+    public class EndpointConstants
+    {
+        public const string LoginPath = "/UserIdentity/Login";
+        public const string LogoutPath = "/UserIdentity/Logout";
+        public const string UserClaimsPath = "/UserIdentity/BFFUser";
+    }
+}
